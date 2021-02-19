bef72bcf89e333512ebff46516d71e00c4adbea6
Will Burklund <waburklund@gmail.com>
2021-02-18 22:18:34 -0600

    Add deploy stage

---
+++ pipeline/stage5_deploy/deploy.go
@@ -0,0 +1,61 @@
+/*
+  WGU Capstone Project
+  Copyright (C) 2021 Will Burklund
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+	"os"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+func HandleRequest(ctx context.Context, input string) (string, error) {
+	// TODO: Move hard-coded strings into environment variables
+	// TODO: Trigger model refresh on API server
+
+	sourceBucket := "capstone-deploy-artifacts"
+	key := "model.h5"
+	destinationBucket := "capstone-api-assets"
+
+	sess, err := session.NewSession()
+	svc := s3.New(sess)
+	_, err = svc.CopyObject(&s3.CopyObjectInput{Bucket: aws.String(destinationBucket),
+		CopySource: aws.String(url.PathEscape(sourceBucket + "/" + key)), Key: aws.String(key)})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to copy item from bucket %q to bucket %q, %v\n", sourceBucket, destinationBucket, err)
+		os.Exit(1)
+	}
+
+	err = svc.WaitUntilObjectExists(&s3.HeadObjectInput{Bucket: aws.String(destinationBucket), Key: aws.String(key)})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error occurred while waiting for item %q to be copied to bucket %q, %v\n", key, destinationBucket, err)
+		os.Exit(1)
+	}
+
+	return fmt.Sprintf("Deploy successful."), nil
+}
+
+func main() {
+	lambda.Start(HandleRequest)
+}
---
+++ pipeline/stage5_deploy/go.mod
@@ -0,0 +1,8 @@
+module deploy
+
+go 1.16
+
+require (
+	github.com/aws/aws-lambda-go v1.22.0
+	github.com/aws/aws-sdk-go v1.37.14
+)
