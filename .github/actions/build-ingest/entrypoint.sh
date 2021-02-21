6cec9203ee5aa10aefe37e52a09b3dfc55296e36
Will Burklund <waburklund@gmail.com>
2021-02-21 11:30:46 -0600

    Finalize ZIP naming

--- .github/actions/build-ingest/entrypoint.sh
+++ .github/actions/build-ingest/entrypoint.sh
@@ -2,4 +2,4 @@
 
 cd /github/workspace/pipeline/stage1_ingest/
 dotnet tool restore
-dotnet lambda package -o /github/workspace/ingest.zip
+dotnet lambda package -o /github/workspace/stage1_ingest.zip
