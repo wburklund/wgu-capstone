df8752d5d08ab8d219113d9139f5866718c58de6
Will Burklund <waburklund@gmail.com>
2021-02-21 14:25:20 -0600

    Clean up build-clean script

--- .github/actions/build-clean/entrypoint.sh
+++ .github/actions/build-clean/entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
 cd /github/workspace/pipeline/stage2_clean/
-cargo build
-dotnet tool restore
-dotnet lambda package -o /github/workspace/stage2_clean.zip
-#!/bin/sh
-
-cd /github/workspace/pipeline/stage2_clean/
 cargo build --release
 zip /github/workspace/stage2_clean.zip target/release/bootstrap
