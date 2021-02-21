139df46c35015fc173f0cef7dc12a35e236cf3f6
Will Burklund <waburklund@gmail.com>
2021-02-21 14:48:43 -0600

    Fix path

--- .github/actions/build-model/entrypoint.sh
+++ .github/actions/build-model/entrypoint.sh
@@ -6,5 +6,5 @@ cd /github/workspace/pipeline/stage3_model/
 
 cp run/* /github/workspace/artifacts/stage3_model_run/
 zip /github/workspace/artifacts/stage3_model_status.zip status/index.js
-zip /github/workspace/artifacts/stage3_model_trigger.zip status/lambda_function.rb
+zip /github/workspace/artifacts/stage3_model_trigger.zip trigger/lambda_function.rb
  
