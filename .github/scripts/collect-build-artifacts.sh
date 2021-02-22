2442fe83bc5d9036b39e4253013ba63f23c323ca
Will Burklund <waburklund@gmail.com>
2021-02-21 20:31:03 -0600

    Change around command format

--- .github/scripts/collect-build-artifacts.sh
+++ .github/scripts/collect-build-artifacts.sh
@@ -4,8 +4,18 @@ set -x
 
 mkdir -p artifacts
 
-[ -d "stage1_ingest" ] && mv stage1_ingest/* artifacts/
-[ -d "stage2_clean" ] && mv stage2_clean/* artifacts/
-[ -d "stage3_model" ] && mv stage3_model/* artifacts/
-[ -d "stage4_test" ] && mv stage4_test/* artifacts/
-[ -d "stage5_deploy" ] && mv stage5_deploy/* artifacts/
+if [ -d "stage1_ingest" ]; then
+    mv stage1_ingest/* artifacts/
+fi
+if [ -d "stage2_clean" ]; then
+    mv stage2_clean/* artifacts/
+fi
+if [ -d "stage3_model" ]; then
+    mv stage3_model/* artifacts/
+fi
+if [ -d "stage4_test" ]; then
+    mv stage4_test/* artifacts/
+fi
+if [ -d "stage5_deploy" ]; then
+    mv stage5_deploy/* artifacts/
+fi
