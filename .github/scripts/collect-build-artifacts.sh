14ce88291d948bf36a4dcfd5f9ff85943e39d4ff
Will Burklund <waburklund@gmail.com>
2021-02-21 19:28:44 -0600

    Fix workflow

---
+++ .github/scripts/collect-build-artifacts.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+mkdir -p artifacts
+
+[ -d "stage1_ingest" ] && mv stage1_ingest/* artifacts/
+[ -d "stage2_clean" ] && mv stage2_clean/* artifacts/
+[ -d "stage3_model" ] && mv stage3_model/* artifacts/
+[ -d "stage4_test" ] && mv stage4_test/* artifacts/
+[ -d "stage5_deploy" ] && mv stage5_deploy/* artifacts/
+
+ls artifacts
