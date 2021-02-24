a4c376ea8533df2add2b9086fc755d29fdae3741
Will Burklund <waburklund@gmail.com>
2021-02-23 18:50:51 -0600

    Attach txt extension

--- .github/scripts/hash-zips.sh
+++ .github/scripts/hash-zips.sh
@@ -2,5 +2,5 @@
 
 for zip in $(find -name "*.zip")
 do
-        openssl dgst -sha256 -binary $zip | openssl enc -base64 > $zip.sha256
+        openssl dgst -sha256 -binary $zip | openssl enc -base64 > $zip.sha256.txt
 done
