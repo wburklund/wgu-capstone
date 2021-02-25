1ae6acc914fdb7653d055678b6ab82a7a87dfa59
Will Burklund <waburklund@gmail.com>
2021-02-25 07:51:41 -0600

    Move timeout to script

--- .github/scripts/awscurl-retry.sh
+++ .github/scripts/awscurl-retry.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-timeout "$3" sh -c "until awscurl --service execute-api -X $1 $2; do sleep $4; done"
+timeout "$3" sh -c "until awscurl --service execute-api -X $1 $2; do sleep 1; done"
