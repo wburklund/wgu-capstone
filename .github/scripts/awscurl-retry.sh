ba89420a0b20e3943142274e2f5dfe9fa1e72738
Will Burklund <waburklund@gmail.com>
2021-02-24 21:22:58 -0600

    Remove extra fluff from timeout parameter

--- .github/scripts/awscurl-retry.sh
+++ .github/scripts/awscurl-retry.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-while timeout -k $3+10 $3 -- awscurl --service execute-api -X $1 $2; [ $? = 124 ]
+while timeout -k 10 $3 -- awscurl --service execute-api -X $1 $2; [ $? = 124 ]
 do sleep $4
 done
