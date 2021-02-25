a7e673bf07858807ff02dfe51c228f38ecdfe2b3
Will Burklund <waburklund@gmail.com>
2021-02-24 18:16:15 -0600

    Add semicolons

--- infra/assets/bootstrap_model.sh
+++ infra/assets/bootstrap_model.sh
@@ -1,6 +1,6 @@
-[ -r "/home/ec2-user" ] || sudo chmod -R 777 /home/ec2-user
-[ -d "~/anaconda3" ] || ln -s /home/ec2-user/anaconda3 ~/anaconda3
-[ -d "~/.dl_binaries" ] || ln -s /home/ec2-user/.dl_binaries ~/.dl_binaries
-aws s3 sync --delete s3://capstone-code-store/stage3_model_run/ ~/code
-chmod +x ~/code/run.sh
+[ -r "/home/ec2-user" ] || sudo chmod -R 777 /home/ec2-user;
+[ -d "~/anaconda3" ] || ln -s /home/ec2-user/anaconda3 ~/anaconda3;
+[ -d "~/.dl_binaries" ] || ln -s /home/ec2-user/.dl_binaries ~/.dl_binaries;
+aws s3 sync --delete s3://capstone-code-store/stage3_model_run/ ~/code;
+chmod +x ~/code/run.sh;
 cd ~/code; ./run.sh
