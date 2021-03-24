807c94fe6fbf22643cdcba8423eee35d5a9778cd
Will Burklund <waburklund@gmail.com>
2021-03-23 21:22:17 -0500

    Polish legend, time axis

--- application/web/src/Report/index.js
+++ application/web/src/Report/index.js
@@ -17,7 +17,7 @@
 */
 
 import React from 'react';
-import { Header, Segment, Button } from 'semantic-ui-react';
+import { Segment } from 'semantic-ui-react';
 import {
   XYPlot,
   XAxis,
@@ -25,53 +25,53 @@ import {
   ChartLabel,
   HorizontalGridLines,
   VerticalGridLines,
-  LineSeriesCanvas
+  LineSeriesCanvas,
+  DiscreteColorLegend
 } from 'react-vis';
 
 const Line = LineSeriesCanvas;
 
 class Report extends React.Component {
-    render() {
-        return (
-            <Segment placeholder style={{ height: '100%', width: '100%', paddingTop: '15px' }}>
-        <XYPlot width={800} height={600}>
+  render() {
+    return (
+      <Segment placeholder style={{ height: '100%', width: '100%', paddingTop: '15px' }}>
+        <XYPlot width={800} height={600} xType='time'>
           <HorizontalGridLines />
           <VerticalGridLines />
-          <XAxis />
+          <XAxis tickTotal={6}/>
           <YAxis />
-          <ChartLabel 
+          <DiscreteColorLegend items={['Normal', 'Virus', 'Bacteria', 'Smoking']} orientation='horizontal' />
+          <ChartLabel
             text="Time"
             className="alt-x-label"
             includeMargin={false}
             xPercent={0.5}
             yPercent={1.01}
-            />
-
-          <ChartLabel 
+          />
+          <ChartLabel
             text="Stock Price"
             className="alt-y-label"
             includeMargin={false}
             xPercent={0.02}
             yPercent={0.5}
-            style={{
-              transform: 'rotate(-90)',
-            }}
-            />
+            style={{transform: 'rotate(-90)'}}
+          />
           <Line
             className="first-series"
-            data={[{x: 1, y: 30}, {x: 2, y: 40}, {x: 3, y: 60}, {x: 4, y: 120}]}
+            data={[{ x: new Date('01/01/2021'), y: 30 }, { x: new Date('01/15/2021'), y: 40 }, { x: new Date('01/29/2021'), y: 60 }, { x: new Date('02/13/2021'), y: 120 }]}
           />
-          <Line className="second-series" data={null} />
+          <Line className="second-series" data={[{ x: new Date('01/01/2021'), y: 25 }, { x: new Date('01/15/2021'), y: 35 }, { x: new Date('01/29/2021'), y: 55 }, { x: new Date('02/13/2021'), y: 100 }]} />
           <Line
             className="third-series"
             curve={'curveMonotoneX'}
-            data={[{x: 1, y: 30}, {x: 2, y: 40}, {x: 3, y: 60}, {x: 4, y: 120}]}
+            data={[{ x: new Date('01/01/2021'), y: 30 }, { x: new Date('01/15/2021'), y: 40 }, { x: new Date('01/29/2021'), y: 60 }, { x: new Date('02/13/2021'), y: 120 }]}
             strokeDasharray={[7, 3]}
           />
+          <Line className="second-series" data={[{ x: new Date('01/01/2021'), y: 10 }, { x: new Date('01/15/2021'), y: 15 }, { x: new Date('01/29/2021'), y: 20 }, { x: new Date('02/13/2021'), y: 25 }]} />
         </XYPlot>
-            </Segment>
-        )
-    }
+      </Segment>
+    )
+  }
 }
 
 export default Report;
