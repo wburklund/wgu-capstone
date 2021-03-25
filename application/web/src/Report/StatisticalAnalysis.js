264bb0bcbed4fe80305c4a34265d2dc8a4f336e7
Will Burklund <waburklund@gmail.com>
2021-03-25 16:39:41 -0500

    Basic statistics

--- application/web/src/Report/StatisticalAnalysis.js
+++ application/web/src/Report/StatisticalAnalysis.js
@@ -17,47 +17,114 @@
 */
 
 import React from 'react';
-import { Header, Dropdown } from 'semantic-ui-react';
+import { Header, Dropdown, MenuItem, List, Grid } from 'semantic-ui-react';
 
 const statisticOptions = [
-    {
-      text: 'Normal',
-      value: 'Normal'
-    },
-    {
-      text: 'Virus',
-      value: 'Virus'
-    },
-    {
-      text: 'Bacteria',
-      value: 'Bacteria'
-    }
+  {
+    text: 'Normal',
+    value: 'Normal'
+  },
+  {
+    text: 'Virus',
+    value: 'Virus'
+  },
+  {
+    text: 'Bacteria',
+    value: 'Bacteria'
+  }
 ]
 
 const initialState = {
-    selectedStat: 'Normal'
+  selectedStat: 'Normal'
 }
 
 class StatisticalAnalysis extends React.Component {
-    state = initialState
+  state = initialState
 
-    render() {
-        let { data } = this.props;
-        let { selectedStat } = this.state;
-
-        return (
-            <React.Fragment>
-                <br />
-                <br />
-                <Header>Statistical Analysis</Header>
-                <Dropdown
-                selection
-                options={statisticOptions}
-                defaultValue={'Normal'}
-                onChange={(_, d) => this.setState({ 'selectedStat': d.value })} />
-            </React.Fragment>
-        )
+  render() {
+    let { data } = this.props;
+    let { selectedStat } = this.state;
+    let selectedData = data[selectedStat];
+    if (!selectedData) {
+      return <div />
     }
+    let mean = selectedData.reduce((a, d) => a + d.y, 0) / selectedData.length
+
+    let sortData = [...selectedData]
+    sortData.sort((a, b) => a.y - b.y)
+    let isOdd = sortData.length % 2 == 1
+    let q1Index = Math.round(sortData.length / 4)
+    let midIndex = Math.floor(sortData.length / 2)
+    let q3Index = Math.round(sortData.length * 3 / 4)
+    let median;
+    if (isOdd) {
+      median = (sortData[midIndex].y + sortData[midIndex + 1].y) / 2
+    } else {
+      median = sortData[midIndex].y
+    }
+    let min = sortData[0].y
+    let max = sortData[sortData.length - 1].y
+    let q1 = sortData[q1Index].y
+    let q3 = sortData[q3Index].y
+
+    return (
+      <React.Fragment>
+        <br />
+        <br />
+        <Header>Statistical Analysis</Header>
+        <br />
+        <Dropdown
+          selection
+          options={statisticOptions}
+          defaultValue={'Normal'}
+          onChange={(_, d) => this.setState({ 'selectedStat': d.value })} />
+        <br />
+        <br />
+        <br />
+
+        <Grid columns={2}>
+          <Grid.Column style={{ position: 'relative' }}>
+            <List celled size="big">
+              <List.Item>
+                <List.Content>
+                  <List.Header>Mean</List.Header>
+                  {mean.toFixed(2)} cases/day
+                    </List.Content>
+              </List.Item>
+              <List.Item>
+                <List.Content>
+                  <List.Header>Median</List.Header>
+                  {median.toFixed(2)} cases/day
+                    </List.Content>
+              </List.Item>
+              <List.Item>
+                <List.Content>
+                  <List.Header>Range</List.Header>
+                  {min}-{max} cases/day
+                    </List.Content>
+              </List.Item>
+              <List.Item>
+                <List.Content>
+                  <List.Header>First Quartile</List.Header>
+                  {q1} cases/day
+                    </List.Content>
+              </List.Item>
+              <List.Item>
+                <List.Content>
+                  <List.Header>Third Quartile</List.Header>
+                  {q3} cases/day
+                    </List.Content>
+              </List.Item>
+            </List>
+          </Grid.Column>
+          <Grid.Column>
+            {JSON.stringify(data[selectedStat].slice(0, 20))}
+          </Grid.Column>
+        </Grid>
+
+      </React.Fragment>
+    )
+  }
 }
 
 export default StatisticalAnalysis;
